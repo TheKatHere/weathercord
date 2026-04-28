bd06942d82d409d499d17325736fadd7b2f15a14
raynecloudy <hi@rayne.page>
2026-04-28 16:18:29 -0700

    fix everything

--- src/app/u/[username]/route.ts
+++ src/app/u/[username]/route.ts
@@ -3,8 +3,6 @@ import cryptoRandomString from "crypto-random-string";
 import { db } from "@/db";
 import { count, eq } from "drizzle-orm";
 import { hash } from "bcrypt";
-import { useRouter } from "next/router";
-import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
 interface POSTBody {
@@ -14,25 +12,26 @@ interface POSTBody {
 
 const generateUID = (sequential: number) => `${Math.floor(sequential).toString(16)}w${Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString(16).padStart(14, "0")}`;
 
-export const POST = async (req: Request) => {
-  const router = useRouter();
-  const username = router.query.username as string;
+export async function POST(req: Request, { params }: { params: Promise<{ username: string }> }) {
+  const username = (await params).username;
 
   const { email, password }: Partial<POSTBody> = await req.json();
 
-  // if (!email) return new NextResponse("Email is required", { status: 400 });
-  if (!password) return new NextResponse("Password is required", { status: 400 });
+  // if (!email) return new Response("Email is required", { status: 400 });
+  if (!password) return new Response("Password is required", { status: 400 });
+  if (!username) return new Response("Username is required", { status: 400 });
 
-  if (username.length < 1) return new NextResponse("Username is too short", { status: 400 });
-  if (username.length > 20) return new NextResponse("Username is too long", { status: 400 });
+  if (username.length < 1) return new Response("Username is too short", { status: 400 });
+  if (username.length > 20) return new Response("Username is too long", { status: 400 });
 
   // if (email.length > 40) return new NextResponse("Email is too long", { status: 400 });
 
-  if (password.length < 8) return new NextResponse("Password is too short", { status: 400 });
-  if (password.length > 50) return new NextResponse("Password is too long", { status: 400 });
+  if (password.length < 8) return new Response("Password is too short", { status: 400 });
+  if (password.length > 50) return new Response("Password is too long", { status: 400 });
 
-  const usernameTaken = !!db.select().from(accountsTable).where(eq(accountsTable.username, username));
-  if (usernameTaken) return new NextResponse("Username is taken", { status: 400 });
+  const usernameTaken = (await db.select().from(accountsTable).where(eq(accountsTable.username, username))).values().toArray()[0];
+  console.log(usernameTaken);
+  if (usernameTaken) return new Response("Username is taken", { status: 400 });
 
   const id = generateUID((await db.select({ count: count() }).from(accountsTable))[0].count);
 
@@ -46,13 +45,13 @@ export const POST = async (req: Request) => {
   });
 
   const passwordHash = await hash(password, 10);
-  db.insert(accountsTable).values({
+  await db.insert(accountsTable).values({
     email: "",
     id,
     joined: Date.now(),
     password: passwordHash,
     username
-  });
+  }).execute();
 
-  return new NextResponse();
+  return new Response();
 }
--- src/components/SignUpModal/SignUpModal.tsx
+++ src/components/SignUpModal/SignUpModal.tsx
@@ -1,45 +1,90 @@
 "use client";
 
+import { useState } from "react";
 import Modal from "../Modal/Modal";
 
-const SignUpModal = () => {
-  const pictures = 10;
+const pictures = 10;
 
-  let picture1 = Math.floor(Math.random() * pictures);
-  let picture2;
-  do {
-    picture2 = Math.floor(Math.random() * pictures);
-  } while (picture1 === picture2);
+let picture1 = Math.floor(Math.random() * pictures);
+let picture2;
+do {
+  picture2 = Math.floor(Math.random() * pictures);
+} while (picture1 === picture2);
+
+let picture1Top = Math.random();
+let picture1Width = Math.random();
+let picture2Top = Math.random();
+let picture2Width = Math.random();
+
+const Pictures = () => (
+  <>
+    <img className="rounded-2xl absolute z-[-1] -translate-x-1/2 -translate-y-1/2 transition hover:scale-105" src={"/signup/" + picture1 + ".png"} aria-hidden style={{
+      top: `calc(55% + ${picture1Top * 10}%)`,
+      left: "calc(50% - 15rem)",
+      width: `${picture1Width * 7 + 14}rem`
+    }} />
+    <img className="rounded-2xl absolute z-[-1] -translate-x-1/2 -translate-y-1/2 transition hover:scale-105" src={"/signup/" + picture2 + ".png"} aria-hidden style={{
+      top: `calc(35% + ${picture2Top * 10}%)`,
+      left: "calc(50% + 15rem)",
+      width: `${picture2Width * 7 + 14}rem`
+    }} />
+  </>
+);
+
+const SignUpModal = () => {
+  let [username, setUsername] = useState("");
+  let [password, setPassword] = useState("");
+  let [password2, setPassword2] = useState("");
+
+  let [error, setError] = useState("");
+
   return (
     <>
       <Modal>
-        <img className="rounded-2xl absolute z-[-1] -translate-x-1/2 -translate-y-1/2 transition hover:scale-105" src={"/signup/" + picture1 + ".png"} aria-hidden style={{
-          top: `calc(55% + ${Math.random() * 10}%)`,
-          left: "calc(50% - 15rem)",
-          width: `${Math.random() * 7 + 14}rem`
-        }} />
-        <img className="rounded-2xl absolute z-[-1] -translate-x-1/2 -translate-y-1/2 transition hover:scale-105" src={"/signup/" + picture2 + ".png"} aria-hidden style={{
-          top: `calc(35% + ${Math.random() * 10}%)`,
-          left: "calc(50% + 15rem)",
-          width: `${Math.random() * 7 + 14}rem`
-        }} />
+        <Pictures />
         <img className="w-12 m-auto" src="/Weathercord.svg" alt="Weathercord" />
         <h1 className="text-center">Join Weathercord</h1>
-        <form>
+        <form onSubmit={async (event) => {
+          event.preventDefault();
+          if (password !== password2) {
+            setError("Passwords don't match");
+            return;
+          }
+
+          const res = await fetch(`/u/create`, {
+            method: "POST",
+            body: JSON.stringify({
+              username,
+              password
+            })
+          });
+
+          if (!res.ok) {
+            error = await res.text();
+            return;
+          }
+        }}>
           <label>
             <div>Username</div>
-            <input type="text" />
+            <input type="text" value={username} onChange={(event) => setUsername(event.currentTarget.value)} />
           </label>
           <label>
             <div>Password</div>
-            <input type="password" />
+            <input type="password" value={password} onChange={(event) => setPassword(event.currentTarget.value)} />
           </label>
           <label>
             <div>Re-type Password</div>
-            <input type="password" />
+            <input type="password" value={password2} onChange={(event) => setPassword2(event.currentTarget.value)} />
           </label>
           <input type="submit" value="Create Account" />
         </form>
+        <div style={{
+          boxSizing: "content-box",
+          height: `${error.length > 0 ? 1 : 0}lh`,
+          overflow: "hidden",
+          paddingTop: `${error.length > 0 ? 1 : 0}rem`,
+          transition: "0.25s"
+        }}>{error}</div>
       </Modal>
     </>
   );
